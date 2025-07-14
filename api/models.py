f73eee9b94d3e4b15a0b67fb7c3a642e43957897
angelobenhanan <angelo.benhanan@ui.ac.id>
2025-07-14 23:42:36 +0700

    Changed the Genre and Buku models

--- api/models.py
+++ api/models.py
@@ -1,23 +1,25 @@
 from django.db import models
 
 class Perpustakaan(models.Model):
-    nama_perpustakaan = models.CharField(max_length=100, editable=True)
-    alamat_perpustakaan = models.CharField(max_length=1000, editable=True)
+    nama_perpustakaan = models.CharField(max_length=100)
+    alamat_perpustakaan = models.TextField()
 
     def __str__(self):
         return self.nama_perpustakaan
 
 class Genre(models.Model):
     nama_genre = models.CharField(max_length=1000, editable=True)
-    daftar_buku = []
 
     def __str__(self):
         return self.nama_genre
 
+    def daftar_buku(self):
+        return self.buku_set.all()
+
 class Buku(models.Model):
-    judul = models.CharField(max_length=1000, editable=True)
-    pengarang = models.CharField(max_length=1000, editable=True)
-    genre = models.ForeignKey(Genre, on_delete = models.CASCADE)
+    judul = models.CharField(max_length=1000)
+    pengarang = models.CharField(max_length=1000)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.judul
--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from .views import PerpustakaanListCreate
 
 urlpatterns = [
     path('perpustakaan/', views.PerpustakaanListCreate.as_view(), name='perpustakaan_view_create'),
+    path('buku/', views.BukuListCreate.as_view(), name='buku_view_create'),
+    path('genre/', views.GenreListCreate.as_view(), name='genre_view_create'),
 ]
