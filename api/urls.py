5e4bface30e18f9ff5d4c0a3fb3310123eefb7f8
angelobenhanan <angelo.benhanan@ui.ac.id>
2025-07-15 16:36:41 +0700

    Changed the GET, POST, PUT, and DELETE methods from using generics to using api_view

--- api/urls.py
+++ api/urls.py
@@ -2,14 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('perpustakaan/', views.PerpustakaanCreateView.as_view(), name='perpustakaan_view_create'),
-    path('perpustakaan/<id>', views.PerpustakaanRetrieveUpdateDestroy.as_view(), name='perpustakaan_retrieve_update_delete'),
-    path('perpustakaan/<id>/buku', views.BukuInPerpustakaanView.as_view(), name='perpustakaan_daftar_buku'),
-    path('buku/', views.BukuCreateView.as_view(), name='buku_view_create'),
-    path('buku/<id>', views.BukuRetrieveUpdateDestroy.as_view(), name='buku_retrieve_update_delete'),
-    path('genre/', views.GenreCreateView.as_view(), name='genre_view_create'),
-    path('genre/<id>', views.GenreRetrieve.as_view(), name='genre_retrieve'),
-    path('genre/<id>/delete', views.GenreDelete.as_view(), name='genre_delete'),
-    path('genre/<id>/buku', views.BukuWithGenreView.as_view(), name='genre_daftar_buku'),
+    # URLS using api_view
+    path('perpustakaan/', views.get_post_perpustakaan, name='get_post_perpustakaan'),
+    path('perpustakaan/<pk>', views.perpustakaan_detail, name='get_put_del_perpustakaan'),
+    path('perpustakaan/<pk>/buku', views.buku_in_perpustakaan, name='get_buku_in_perpustakaan'),
+    path('buku/', views.get_post_buku, name='get_post_buku'),
+    path('buku/<pk>', views.buku_detail, name='get_put_del_buku'),
+    path('genre/', views.get_post_genre, name='get_post_genre'),
+    path('genre/<pk>', views.genre_detail, name='get_del_genre'),
+    path('genre/<pk>/buku', views.buku_w_genre, name='get_buku_w_genre'),
+
+    # URLS using the generics methods
+    # path('perpustakaan/', views.PerpustakaanCreateView.as_view(), name='perpustakaan_view_create'),
+    # path('perpustakaan/<id>', views.PerpustakaanRetrieveUpdateDestroy.as_view(), name='perpustakaan_retrieve_update_delete'),
+    # path('perpustakaan/<id>/buku', views.BukuInPerpustakaanView.as_view(), name='perpustakaan_daftar_buku'),
+    # path('buku/', views.BukuCreateView.as_view(), name='buku_view_create'),
+    # path('buku/<id>', views.BukuRetrieveUpdateDestroy.as_view(), name='buku_retrieve_update_delete'),
+    # path('genre/', views.GenreCreateView.as_view(), name='genre_view_create'),
+    # path('genre/<id>', views.GenreRetrieve.as_view(), name='genre_retrieve'),
+    # path('genre/<id>/delete', views.GenreDelete.as_view(), name='genre_delete'),
+    # path('genre/<id>/buku', views.BukuWithGenreView.as_view(), name='genre_daftar_buku'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,144 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import *
 
+# Methods w/ api_view
+# GET all, POST perpus
+@api_view(['GET', 'POST'])
+def get_post_perpustakaan(request):
+    if request.method == 'GET':
+        perpustakaan = Perpustakaan.objects.all()
+        serializer = PerpustakanSerializer(perpustakaan, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = PerpustakanSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# GET, POST, DELETE specific perpus
+@api_view(['GET', 'PUT', 'DELETE'])
+def perpustakaan_detail(request, pk):
+    try:
+        perpustakaan = Perpustakaan.objects.get(pk=pk)
+    except Perpustakaan.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PerpustakanSerializer(perpustakaan)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = PerpustakanSerializer(perpustakaan, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        perpustakaan.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# GET all, POST buku
+@api_view(['GET', 'POST'])
+def get_post_buku(request):
+    if request.method == 'GET':
+        buku = Buku.objects.all()
+        serializer = BukuSerializer(buku, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BukuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# GET, POST, DELETE specific buku
+@api_view(['GET', 'PUT', 'DELETE'])
+def buku_detail(request, pk):
+    try:
+        buku = Buku.objects.get(pk=pk)
+    except Buku.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BukuSerializer(buku)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = BukuSerializer(buku, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        buku.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# GET all buku in specific perpus
+@api_view(['GET'])
+def buku_in_perpustakaan(request, pk):
+    perpustakaan = Perpustakaan.objects.get(pk=pk)
+    if Perpustakaan.objects.filter(pk=pk).exists():
+        buku_list = Buku.objects.all().filter(perpustakaan=perpustakaan)
+        serializer = BukuSerializer(buku_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+# GET all, POST genre
+@api_view(['GET', 'POST'])
+def get_post_genre(request):
+    if request.method == 'GET':
+        genre = Genre.objects.all()
+        serializer = GenreSerializer(genre, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# GET, POST, DELETE specific genre
+@api_view(['GET', 'DELETE'])
+def genre_detail(request, pk):
+    try:
+        genre = Genre.objects.get(pk=pk)
+    except Genre.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        genre.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# GET all buku w/ specific genre
+@api_view(['GET'])
+def buku_w_genre(request, pk):
+    genre = Genre.objects.get(pk=pk)
+    if Genre.objects.filter(pk=pk).exists():
+        buku_list = Buku.objects.all().filter(genre=genre)
+        serializer = BukuSerializer(buku_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+# Methods w/ generics
 # Buku GET all & POST
 class PerpustakaanCreateView(generics.ListCreateAPIView):
     queryset = Perpustakaan.objects.all()
